7bf8a7ab8379fdf5f2cc6ba34247b7eb9f622293
Clarissa <klary.ardon@gmail.com>
2022-05-27 14:58:53 +0200

    initial files setup

---
+++ junior-frontend-react-native/app/src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, ScrollView, Text } from 'react-native'
+
+export const HomeScreen = () => {
+  return (
+      <SafeAreaView>
+        <ScrollView>
+            <Text>Home Screen</Text>
+        </ScrollView>
+      </SafeAreaView>
+  )
+}
